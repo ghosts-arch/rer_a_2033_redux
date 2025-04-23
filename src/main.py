d79a330c4bd8747a3f6a3a3e77ada2e2e0d19cbe
ghosts-arch <ronan.questier@mailfence.com>
2025-04-24 00:04:08 +0200

    add main menu

--- src/main.py
+++ src/main.py
@@ -21,6 +21,83 @@ ENNEMY_COLOR: tuple[int, int, int] = (255, 0, 0)
 ENNEMY_SPEED: int = 1
 
 
+def game(screen) -> bool:
+    ADD_ENNEMY_EVENT = pygame.USEREVENT + 1
+    pygame.time.set_timer(ADD_ENNEMY_EVENT, 5000)
+    is_running: bool = True
+    clock = pygame.time.Clock()
+    player: pygame.Rect = pygame.Rect(
+        player_position[0],
+        player_position[1],
+        PLAYER_WIDTH,
+        PLAYER_HEIGHT,
+    )
+    ennemies: list[pygame.Rect] = []
+    bullets: list[pygame.Rect] = []
+    while is_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    print("space pressed")
+                bullet: pygame.Rect = pygame.Rect(
+                    player_position[0], player_position[1] + player.width // 2, 5, 5
+                )
+                bullets.append(bullet)
+            elif event.type == ADD_ENNEMY_EVENT:
+                ennemy: pygame.Rect = pygame.Rect(64, 400, ENNEMY_WIDTH, ENNEMY_HEIGHT)
+                pygame.draw.rect(screen, ENNEMY_COLOR, ennemy)
+                ennemies.append(ennemy)
+
+        screen.fill((30, 30, 30))
+
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT] and player_position[0] > 0:
+            player.move_ip(-5, 0)
+        if key[pygame.K_RIGHT] and player_position[0] < SCREEN_WIDTH - PLAYER_WIDTH:
+            player.move_ip(5, 0)
+
+        # move all bullets
+        for bullet in bullets:
+            bullet.move_ip(-1, 0)
+            if bullet.x < 0:
+                bullets.remove(bullet)
+                print("bullet removed")
+        # move all ennemies
+        for ennemy in ennemies:
+            ennemy.move_ip(1, 0)
+
+        # check collisions between bullets and ennemies
+        collied_bullets = []
+        collied_ennemies = []
+
+        for ennemy in ennemies:
+            for bullet in bullets:
+                if ennemy.colliderect(bullet):
+                    collied_ennemies.append(ennemy)
+                    collied_bullets.append(bullet)
+            if ennemy.colliderect(player):
+                is_running = False
+
+        # remove bullets
+        bullets = [bullet for bullet in bullets if bullet not in collied_bullets]
+        # remove ennemies
+        ennemies = [ennemy for ennemy in ennemies if ennemy not in collied_ennemies]
+        # draw bullets
+        for bullet in bullets:
+            pygame.draw.rect(screen, (0, 255, 0), bullet)
+        # draw ennemies
+        for ennemy in ennemies:
+            pygame.draw.rect(screen, ENNEMY_COLOR, ennemy)
+
+        pygame.draw.rect(screen, PLAYER_COLOR, player)
+
+        clock.tick(FPS)
+        pygame.display.update()
+    return is_running
+
+
 def main() -> None:
     print("Hello from rer-a-2033-redux!")
     pygame.init()
@@ -31,20 +108,37 @@ def main() -> None:
     ADD_ENNEMY_EVENT = pygame.USEREVENT + 1
     pygame.time.set_timer(ADD_ENNEMY_EVENT, 5000)
 
-    ennemies: list[pygame.Rect] = []
-    bullets: list[pygame.Rect] = []
     clock = pygame.time.Clock()
 
     is_running: bool = True
 
-    player: pygame.Rect = pygame.Rect(
-        player_position[0],
-        player_position[1],
-        PLAYER_WIDTH,
-        PLAYER_HEIGHT,
+    font = pygame.font.Font(None, 36)
+
+    main_title = font.render("RER A 2033 Redux", True, (255, 255, 255))
+    main_title_rectangle = main_title.get_rect(center=(400, 150))
+
+    start_text = font.render(
+        "Appuyer sur `entrée`pour commencer...", True, (255, 255, 255)
     )
+    start_text_rectangle = start_text.get_rect(center=(400, 300))
 
     while is_running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_running = False
+
+        screen.fill((100, 100, 100))
+        screen.blit(main_title, main_title_rectangle)
+        screen.blit(start_text, start_text_rectangle)
+
+        key = pygame.key.get_pressed()
+        if key[pygame.K_RETURN]:
+            print("game started...")
+            game(screen)
+        clock.tick(FPS)
+        pygame.display.update()
+        """
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 is_running = False
@@ -71,6 +165,9 @@ def main() -> None:
         # move all bullets
         for bullet in bullets:
             bullet.move_ip(-1, 0)
+            if bullet.x < 0:
+                bullets.remove(bullet)
+                print("bullet removed")
         # move all ennemies
         for ennemy in ennemies:
 
@@ -103,6 +200,7 @@ def main() -> None:
 
         clock.tick(FPS)
         pygame.display.update()
+        """
 
     pygame.quit()
     sys.exit(0)
