bc34ce98fd3c36b74df8d7395f31b9cafc2ffb69
ghosts-arch <ronan.questier@mailfence.com>
2025-05-03 18:59:55 +0200

    add lifebar

--- src/enemy.py
+++ src/enemy.py
@@ -1,5 +1,9 @@
 from typing import Tuple
+
+import pygame
 from entity import Entity
+
+TOTAL_LIFE: int = 3
 
 
 class Enemy(Entity):
@@ -14,8 +18,15 @@ class Enemy(Entity):
         speed: int,
     ) -> None:
         super().__init__(left, top, width, height, color)
+        self.lifebar = pygame.Rect(left, top - 15, width, 5)
         self.speed = speed
-        self.life = 3
+        self.life = TOTAL_LIFE
+
+    def update(self):
+        self.lifebar.width = (((self.life * 100) / TOTAL_LIFE) * 32) / 100
+        pygame.draw.rect(pygame.display.get_surface(), self.color, self.rect)
+        pygame.draw.rect(pygame.display.get_surface(), (255, 0, 0), self.lifebar)
 
     def move_right(self) -> None:
         self.rect.move_ip(self.speed, 0)
+        self.lifebar.move_ip(self.speed, 0)
